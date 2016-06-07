6f769e8b7b8eb36f8756d4d03124a82e7e777cc1
manujith <manujith.nc@gmail.com>
2016-06-07 07:29:38 +0530

    Refactor phase 1

---
+++ src/main/java/org/wso2/carbon/identity/entitlement/endpoint/util/EntitlementEndpointConstants.java
@@ -0,0 +1,21 @@
+package org.wso2.carbon.identity.entitlement.endpoint.util;
+
+/**
+ * Created by manujith on 5/21/16.
+ */
+public class EntitlementEndpointConstants {
+    public static final String JSON = "json";
+    public static final String XML = "xml";
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String APPLICATION_XML = "application/xml";
+    public static final String dateTimeFormat = "yyyy-MM-dd\'T\'HH:mm:ss";
+    public static final String AUTH_TYPE_BASIC = "Basic";
+    public static final String AUTH_TYPE_OAUTH = "Bearer";
+    public static final String AUTH_PROPERTY_PRIMARY = "primary";
+    public static final String AUTH_HEADER_USERNAME = "userName";
+    public static final String AUTH_HEADER_PASSWORD = "password";
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String AUTHENTICATION_TYPE_HEADER = "Auth_Type";
+    public static final String CONTENT_TYPE_HEADER = "Content-Type";
+    public static final String ACCEPT_HEADER = "Accept";
+}
